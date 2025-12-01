863ea4bbb9f4ecfa28f2d4707c9012577b3ebe6e
viktorHadz <vikecah@gmail.com>
2025-12-01 23:17:24 +0000

    more headscratches - this is not my code its copied

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,98 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
+type User struct {
+	Name string `json:"name"`
+}
+
+var userCache = make(map[int]User)
+
+var cacheMutex sync.RWMutex
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome visitor!")
-	})
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleRoot)
 
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("../static/", http.StripPrefix("../static/", fs))
+	mux.HandleFunc("POST /users", createUser)
+	mux.HandleFunc("GET /users/{id}", getUser)
+	mux.HandleFunc("DELETE /users/{id}", deleteUser)
 
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server listening on : 8080")
+	http.ListenAndServe(":8080", mux)
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World\n")
+	fmt.Fprintf(w, "User Cache: %v", userCache)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if _, ok := userCache[id]; !ok {
+		http.Error(w, "user not found", http.StatusBadRequest)
+		return
+	}
+
+	cacheMutex.Lock()
+	delete(userCache, id)
+	cacheMutex.Unlock()
+
+	w.WriteHeader(http.StatusNoContent)
+
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	cacheMutex.RLock()
+	user, ok := userCache[id]
+	if !ok {
+		http.Error(w, "user not found", http.StatusNotFound)
+		return
+	}
+	cacheMutex.RUnlock()
+
+	w.Header().Set("Content-Type", "application/json")
+	j, err := json.Marshal(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if user.Name == "" {
+		http.Error(w, "name is required", http.StatusBadRequest)
+		return
+	}
+
+	cacheMutex.Lock()
+	userCache[len(userCache)+1] = user
+	cacheMutex.Unlock()
+
+	w.WriteHeader(http.StatusNoContent)
 }
